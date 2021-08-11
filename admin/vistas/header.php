514e2b62f92e37f6d70ae3775650d5123512d564
Jhon Erikson Melendrez Huaman <jhonmelendrez@upeu.edu.pe>
2021-08-10 21:41:38 -0500

    jhonC

--- admin/vistas/header.php
+++ admin/vistas/header.php
@@ -265,7 +265,7 @@ if (strlen(session_id()) < 1)
               </a>
             </li>
 
-            <li>
+            <!--<li>
               <a href="ayuda.php">
                 <i class="fa fa-plus-square"></i> <span>Ayuda</span>
                 <small class="label pull-right bg-red">PDF</small>
@@ -276,7 +276,7 @@ if (strlen(session_id()) < 1)
                 <i class="fa fa-info-circle"></i> <span>Acerca De...</span>
                 <small class="label pull-right bg-yellow">IT</small>
               </a>
-            </li>
+            </li>-->
                         
           </ul>
         </section>

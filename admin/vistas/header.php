9e4765b3673e99d2703080926da8db6dc4482225
David Requejo Santacruz <davidrequejo@upeu.edu.pe>
2021-08-13 13:03:02 -0500

    cambiodia134

--- admin/vistas/header.php
+++ admin/vistas/header.php
@@ -62,9 +62,9 @@ if (strlen(session_id()) < 1)
         <!-- Logo -->
         <a href="index2.html" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>AD</b>Ventas</span>
+          <span class="logo-mini"><b>UPEU</b>Vivero</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>ADVentas</b></span>
+          <span class="logo-lg"><b>UPEUVivero</b></span>
         </a>
 
         <!-- Header Navbar: style can be found in header.less -->
